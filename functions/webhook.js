38799eb27d5ad616229fc787641d055c4e04f74c
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2023-03-11 13:56:42 -0800

    Update webhook.js

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,24 +1,65 @@
-exports.handler = function(event, context, callback) {
-  // Verify webhook secret
-  if (event.headers['x-webhook-secret'] !== process.env.WEBHOOK_SECRET) {
-    return callback(null, {
-      statusCode: 401,
-      body: 'Unauthorized'
-    });
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const secret = 'your_webhook_secret_here'; // Replace with your webhook secret
+  if (event.httpMethod === 'POST' && event.path === '/webhook') {
+    let body = event.body;
+    let headers = event.headers;
+    let signature = headers['x-hub-signature'];
+    let event_type = headers['x-github-event'];
+    // Verify webhook signature
+    let hmac = crypto.createHmac('sha1', secret);
+    hmac.update(body);
+    let calculated_signature = 'sha1=' + hmac.digest('hex');
+    if (signature !== calculated_signature) {
+      return {
+        statusCode: 401,
+        body: 'Unauthorized'
+      };
+    }
+    // Parse payload and trigger message box
+    let payload = JSON.parse(body);
+    if (event_type === 'ping') {
+      return {
+        statusCode: 200,
+        body: 'pong'
+      };
+    } else if (event_type === 'push') {
+      let message = 'A new commit was pushed to the repository';
+      triggerMessageBox(message);
+    }
+    // Respond with success status
+    return {
+      statusCode: 200,
+      body: 'OK'
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: 'Not Found'
+    };
   }
-  // Parse payload and trigger message box
-  let payload = JSON.parse(event.body);
-  if (payload.event === 'button-clicked') {
-    triggerMessageBox(payload.message);
-  }
-  // Respond with success status
-  callback(null, {
-    statusCode: 200,
-    body: 'OK'
-  });
 };
 
-function triggerMessageBox(message) {
-  // Replace with code to trigger message box on your website
-  alert(message);
+async function triggerMessageBox(message) {
+  // Replace YOUR_MESSAGEBOX_ENDPOINT with the URL of your message box endpoint
+  var messageboxUrl = 'YOUR_MESSAGEBOX_ENDPOINT';
+  fetch(messageboxUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      event: 'webhook-received',
+      message: message
+    })
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      console.error('Failed to trigger message box');
+    }
+  })
+  .catch(function(error) {
+    console.error('Error triggering message box: ' + error.message);
+  });
 }
