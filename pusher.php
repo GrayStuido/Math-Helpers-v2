ac57ae3c6de698e359c6840c335591243aa8ff31
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2023-03-11 14:17:03 -0800

    Update pusher.php

--- pusher.php
+++ pusher.php
@@ -3,10 +3,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Pusher\Pusher;
 
-$app_id = 'YOUR_APP_ID';
-$app_key = 'YOUR_APP_KEY';
-$app_secret = 'YOUR_APP_SECRET';
-$app_cluster = 'YOUR_CLUSTER';
+$app_id = '1566853';
+$app_key = '7dbedc6f3a6734be2d71';
+$app_secret = '4dc2d8a52e74669b1e41';
+$app_cluster = 'us3';
 
 $pusher = new Pusher($app_key, $app_secret, $app_id, [
   'cluster' => $app_cluster,
